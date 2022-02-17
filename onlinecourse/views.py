dfc36bce76cc81dd5c79284bd28cfb363abbf03b
annanya0703 <94123181+annanya0703@users.noreply.github.com>
2022-02-17 07:19:21 +0530

    Update views.py
    Updated

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -87,6 +87,7 @@ class CourseListView(generic.ListView):
 class CourseDetailView(generic.DetailView):
     model = Course
     template_name = 'onlinecourse/course_detail_bootstrap.html'
+    #Saranghae Anuya
 
 
 def enroll(request, course_id):
